080ef7d05d15fa37a68dd0dffe56539483adf7ef
Esau Matias <esaufreirema]tias@gmail.com>
2022-09-06 13:32:16 -0300

    feat(Paginations) - cria componente de paginação

--- app/src/Pages/Home.js
+++ app/src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import TableArticles from '../Components/TableArticles';
 import Header from '../Components/Header';
+import Paginations from '../Components/Paginations';
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Header />
       <Container>
         <TableArticles />
+        <Paginations />
       </Container>
     </>
   )
